883c54181b0b45cc7f0bbea8e7ea1826e074b62b
Jeferson Antônio <33436941+jefersonantonio@users.noreply.github.com>
2017-11-08 21:08:04 -0200

    Create apache_install.sh

---
+++ apache_install.sh
@@ -0,0 +1,26 @@
+#!/bin/sh
+
+### Apache Instalation ###
+
+echo "Installing Apache Web Server & Conf Server...please wait"
+
+yum -y install httpd mod_ssl
+mv /etc/httpd/conf/httpd.conf /etc/httpd/conf/httpd.conf_default-bkp
+wget s3.com/httpd.conf -P /etc/httpd/conf
+wget s3.com/ssl.conf -P /etc/httpd/conf.d/
+
+service httpd start
+
+### SSL configuration ###
+
+mkdir /etc/httpd/ssl
+openssl req -x509 -nodes -days 365 -newkey rsa:2048 -keyout /etc/httpd/ssl/apache.key -out /etc/httpd/ssl/apache.crt
+
+### Stop Iptables ###
+service iptables status
+service iptables stop
+
+### Restart Apache ## 
+service httpd restart
+
+echo "Done!"
