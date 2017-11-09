1717bfdbf213ce707959954bf69aefb5d90be869
Jeferson Antônio <jefersonantn@gmail.com>
2017-11-09 12:29:43 -0200

    it´s working!

--- apache_install.sh
+++ apache_install.sh
@@ -1,27 +1,32 @@
 #!/bin/sh
 
 ### Apache Instalation ###
-
 echo "Installing Apache Web Server & Conf Server...please wait"
 
-yum -y install httpd mod_ssl
+yum -y install httpd24 mod24_ssl
 mv /etc/httpd/conf/httpd.conf /etc/httpd/conf/httpd.conf_default-bkp
-wget s3.com/httpd.conf -P /etc/httpd/conf
-wget s3.com/ssl.conf -P /etc/httpd/conf.d/
+wget https://s3.com/httpd.conf -P /etc/httpd/conf
+wget https://s3.com/ssl.conf -P /etc/httpd/conf.d/
 
-service httpd start
 
 ### SSL configuration ###
 
 mkdir -p /etc/httpd/ssl
 openssl req -x509 -nodes -days 365 -newkey rsa:2048 -keyout /etc/httpd/ssl/apache.key -out /etc/httpd/ssl/apache.crt -subj "/C=GB/ST=London/L=London/O=Global Security/OU=IT Department/CN=example.com"
 
-### Stop Iptables & check SELINUX###
+
+### Stop Iptables ###
 service iptables status
 service iptables stop
-sestatus
 
-### Restart Apache ## 
-service httpd restart
+### Add hosts/ip to /etc/hosts ###
+echo "10.237.171.12     login.example.com" >> /etc/hosts
+echo "10.237.171.245      login.example.com" >> /etc/hosts
+echo "10.237.171.13     hostname     example.com" >> /etc/hosts
+echo "10.237.171.13     cname.example-pharma.com" >> /etc/hosts
+
+### Restart Apache ##
+chkconfig httpd on
+service httpd start
 
 echo "Done!"
